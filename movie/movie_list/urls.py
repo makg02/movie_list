0cf524728ad2738a13db0158e20b616e5d1227a8
Mack <gayonmarkanthony@gmail.com>
2018-10-02 00:11:39 -0800

    add movie classes

--- movie/movie_list/urls.py
+++ movie/movie_list/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
-from movie_list.views import MovieLists, AddMovie
+from movie_list.views import MovieLists, AddMovie, UpdateMovie, DeleteMovie
 
 urlpatterns = [
     url(r'^$', MovieLists.as_view()),
-    url(r'^/add$', AddMovie.as_view())
+    url(r'^/add', AddMovie.as_view(), name="add_movie"),
+    url(r'^/update', UpdateMovie.as_view(), name="update_movie"),
+    url(r'^/delete/(?P<movie_id>[0-9a-f-]+)', DeleteMovie.as_view(), name="delete_movie" )
 ]
--- movie/movie_list/views.py
+++ movie/movie_list/views.py
@@ -1,5 +1,7 @@
 import couchdb
 import json
+from pprint import pprint
+from django.shortcuts import get_object_or_404, render
 from django.shortcuts import render, HttpResponse
 from django.views.generic import View
 from movie import settings
@@ -7,23 +9,61 @@ from movie import settings
 s = couchdb.Server(settings.COUCH_DSN)
 
 DB_MOVIES = s['movies']
+
 # Create your views here.
 class MovieLists(View):
 
     def get(self, request):
-
         view = DB_MOVIES.view('management/titles', include_docs=True)
-
         movie_titles = []
         for r in view:
             doc = r.doc
             movie_titles.append(doc.copy())
-
-
         return HttpResponse(json.dumps(movie_titles))
+
 
 class AddMovie(View):
 
-    def get(self, request):
-        print settings.COUCH_DSN
-        return HttpResponse('add')
+    def post(self, request):
+
+        if request.method == 'POST':
+
+            #pprint(request.body)
+            data = json.loads(request.body)
+            try:
+                doc_id, rev = DB_MOVIES.save(data)
+            except:
+                print "something happen"
+
+            return HttpResponse(json.dumps(doc_id))
+
+
+class UpdateMovie(View):
+
+    def post(self, request):
+
+        if request.method == 'POST':
+
+            data = json.loads(request.body)
+            doc_id = data.get('_id')
+            doc = DB_MOVIES.get(doc_id)
+            doc['title'] = data.get('title')
+            doc['description'] = data.get('description')
+
+            try:
+                docid, rev = DB_MOVIES.save(doc)
+            except:
+                print "something happen"
+
+            return HttpResponse(json.dumps(rev))
+
+
+class DeleteMovie(View):
+
+    def delete(self, request, movie_id):
+        try:
+            doc = DB_MOVIES.get(movie_id)
+            DB_MOVIES.delete(doc)
+            return HttpResponse(json.dumps('ok'))
+        except:
+            return HttpResponse(json.dumps('error'))
