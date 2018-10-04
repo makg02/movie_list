dee8118016fd1d44ef499ced580bba92f868aa88
Mack <gayonmarkanthony@gmail.com>
2018-10-04 00:29:35 -0800

    add view backend

--- movie/movie_list/views.py
+++ movie/movie_list/views.py
@@ -14,14 +14,20 @@ DB_MOVIES = s['movies']
 class MovieLists(View):
 
     def get(self, request):
-        view = DB_MOVIES.view('management/titles', include_docs=True)
+
+        if 'movies' not in s:
+            db = s.create('movies')
+            return HttpResponse(json.dumps('succesfully create movies db'), 'application/json')
+
+        view = DB_MOVIES.view('_all_docs', include_docs=True)
         movie_titles = []
         for r in view:
             doc = r.doc
-            movie_titles.append(doc.copy())
+            if doc.get('is_active') == True:
+                movie_titles.append(doc.copy())
 
         movie_titles = sorted(movie_titles, key=lambda k: k['title'])
-        return HttpResponse(json.dumps(movie_titles))
+        return HttpResponse(json.dumps(movie_titles), 'application/json')
 
 
 class AddMovie(View):
@@ -37,8 +43,14 @@ class AddMovie(View):
             except:
                 print "something happen"
 
-            return HttpResponse(json.dumps(doc_id))
+            return HttpResponse(json.dumps(doc_id), 'application/json')
 
+
+class ViewMovie(View):
+
+    def get(self, request, movie_id):
+        doc = DB_MOVIES.get(movie_id)
+        return HttpResponse(json.dumps(doc), 'application/json')
 
 class UpdateMovie(View):
 
@@ -57,7 +69,7 @@ class UpdateMovie(View):
             except:
                 print "something happen"
 
-            return HttpResponse(json.dumps(rev))
+            return HttpResponse(json.dumps(rev), 'application/json')
 
 
 class DeleteMovie(View):
@@ -67,6 +79,6 @@ class DeleteMovie(View):
             doc = DB_MOVIES.get(movie_id)
             doc['is_active'] = False
             DB_MOVIES.save(doc)
-            return HttpResponse(json.dumps('ok'))
+            return HttpResponse(json.dumps('ok'), 'application/json')
         except:
-            return HttpResponse(json.dumps('error'))
+            return HttpResponse(json.dumps('error'), 'application/json')
